db98b2a60f760285c16c181e0b1e2f6425ba985f
Dell <Dell@DESKTOP-82ADQUM>
2020-01-25 10:36:10 +0530

    firstDemo

---
+++ GitDemo/src/com/csi/git/GitHubHello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.csi.git;
+
+/**
+ * @author Dell
+ *
+ */
+public class GitHubHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+   System.out.println("Hell Git");
+	}
+
+}
